25fa1a674c30b653964bb54dbc8c64ce93a521b2
Claire Clark <17522030+augustclear@users.noreply.github.com>
2020-11-30 13:34:32 -0600

    rename folder

--- updateserver/createclientdisk.lua
+++ updateserver/createclientdisk.lua
@@ -1,5 +1,15 @@
+--Loads the files for setting up an update client to a floppy disk
 
+local component = require("component")
+fsc = component.filesystem
 
 args = (...)
 
+local setup_files_folder = /usr/setup/
 local disk_path = args[1]
+
+local filelist = fsc.list(setup_files_folder)
+
+for key, value in pairs(filelist) do
+    filesystem.copy(setup_files_folder .. value,disk_path .. value)
+end
